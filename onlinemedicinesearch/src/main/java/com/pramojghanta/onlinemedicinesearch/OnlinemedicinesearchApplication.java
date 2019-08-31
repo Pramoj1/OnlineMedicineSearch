9e55b6bdefe3fafe23b3adafe3aa6bf486a61b58
Pramoj <Pramoj@DESKTOP-T48HRJ6>
2019-08-31 22:47:31 +0530

    OnlineMedicineSearch intial commit

---
+++ onlinemedicinesearch/src/main/java/com/pramojghanta/onlinemedicinesearch/OnlinemedicinesearchApplication.java
@@ -0,0 +1,13 @@
+package com.pramojghanta.onlinemedicinesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinemedicinesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinemedicinesearchApplication.class, args);
+	}
+
+}
